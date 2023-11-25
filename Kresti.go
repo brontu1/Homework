06ab82c09f1686c9c05064174b1a79aca4c3497f
brontu <dchepel100@gmail.com>
2023-11-26 00:30:49 +0300

    Испанские шахматы(исправлено)

--- Kresti.go
+++ Kresti.go
@@ -56,7 +56,7 @@ func emptyFieldCheck(x, y int) bool {
 	return false
 }
 
-func movimientoDelHombre() {
+func movimientoDelHombre() { //Ход мясного мешка
 	var x, y int
 	fmt.Println("Movimiento del hombre, seleccione coordenadas de campo (x, y)")
 	fmt.Scan(&x, &y)
@@ -68,7 +68,7 @@ func movimientoDelHombre() {
 	gameMap[x-1][y-1] = "x"
 }
 
-func progresoDeLaComputadora() {
+func progresoDeLaComputadora() { //Ход компьютера
 	x := rand.Intn(MapSize)
 	y := rand.Intn(MapSize)
 	if !emptyFieldCheck(x, y) {
@@ -80,12 +80,55 @@ func progresoDeLaComputadora() {
 }
 
 func checkWin() bool {
-	if gameMap[MapSize-1][MapSize-1] == "x" {
+	var countHS int                                                           //Переменная счетчика для проверки мясного мешка по вертикали
+	var countCS int                                                           //Переменная счетчика для проверки комьютера по вертикали
+	if gameMap[0][0] == "x" && gameMap[1][1] == "x" && gameMap[2][2] == "x" { //Проверка по основной диагонали
 		fmt.Println("El hombre gano")
 		return true
-	} else if gameMap[MapSize-1][MapSize-1] == "0" {
+	} else if gameMap[0][0] == "0" && gameMap[1][1] == "0" && gameMap[2][2] == "0" {
 		fmt.Println("La computadora gano")
 		return true
+	} else if gameMap[0][2] == "x" && gameMap[1][1] == "x" && gameMap[2][0] == "x" { //Проверка по вторичной диагонали
+		fmt.Println("El hombre gano")
+		return true
+	} else if gameMap[0][2] == "0" && gameMap[1][1] == "0" && gameMap[2][0] == "0" {
+		fmt.Println("La computadora gano")
+		return true
+	}
+	for i := 0; i < MapSize; i++ {
+		var countH int //Переменная счетчика для проверки мясного мешка по горизонтали
+		var countC int //Переменная счетчика для проверки компьютера по горизонтали
+		var y int      //Костыль-счётчик для сохранения j-ого значения для проверки по вертикали
+		if gameMap[i][y] == "x" {
+			countHS++
+			if countHS == 3 {
+				fmt.Println("El hombre gano")
+				return true
+			}
+		} else if gameMap[i][y] == "0" {
+			countCS++
+			if countCS == 3 {
+				fmt.Println("La computadora gano")
+				return true
+			}
+		}
+
+		for j := 0; j < MapSize; j++ {
+			if gameMap[i][j] == "x" {
+				countH++
+				if countH == 3 {
+					fmt.Println("El hombre gano")
+					return true
+				}
+			} else if gameMap[i][j] == "0" {
+				countC++
+				if countC == 3 {
+					fmt.Println("La computadora gano")
+					return true
+				}
+			}
+			y++
+		}
 	}
 	return false
 }
